5f47575ebac464f0ad40178d3bfd3eec30b7bf06
Nick Sagias <nsagias@gmail.com>
2021-08-28 08:09:54 +0000

    Use a function to count vowels in a string

---
+++ kata-3.js
@@ -0,0 +1,26 @@
+const numberOfVowels = function(data) {
+  // Put your solution here
+  let count = 0;
+  let splitData = [];
+  splitData = data.split("")
+  for ( let i = 0; i < data.length; i++) {
+    if (splitData[i] === "a"){
+      count += 1
+    }
+    else if (splitData[i] === "e"){
+      count += 1
+    }
+    else if (splitData[i] === "i"){
+      count += 1
+    }
+    else if (splitData[i] === "o"){
+      count += 1
+    }
+    else if (splitData[i] === "u"){
+      count += 1
+    }
+  }
+  return count;
+};
+
+
